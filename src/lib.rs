5aeaf2b7902bad6d6270ace8315b06cccdf57e95
YaroslavFetisov <yaroslavf222@gmail.com>
2023-11-08 00:08:14 +0200

    lab

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use pest_derive::Parser;
+pub use pest::Parser;
+
+
 peg::parser!{
     pub grammar list_parser() for str {
       rule number() -> u32
@@ -7,3 +11,48 @@ peg::parser!{
         = "[" l:(number() ** ",") "]" { l }
     }
   }
+
+#[derive(Parser)]
+#[grammar = "./grammar.pest"]
+pub struct Grammar;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn field_test() -> anyhow::Result< () > {
+    
+        let pair = Grammar::parse(Rule::field, "131.13")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
+        assert_eq!( pair.as_str(), "131.13" );
+        assert_eq!( pair.as_span().start(), 0 );
+        assert_eq!( pair.as_span().end(), 6 );
+    
+        let pair = Grammar::parse(Rule::field, "x");
+        assert!( pair.is_err() );
+    
+        let pair = Grammar::parse(Rule::field, "");
+        assert!( pair.is_err() );
+    
+        // dbg!(pair);
+        Ok( () )
+    }
+
+
+
+    #[test]
+    fn record_test() -> anyhow::Result< () > {
+    
+        let pair = Grammar::parse(Rule::record, "131.13,99")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
+        assert_eq!( pair.as_str(), "131.13,99" );
+        assert_eq!( pair.as_span().start(), 0 );
+        assert_eq!( pair.as_span().end(), 9 );
+    
+        let pair = Grammar::parse(Rule::record, "");
+        assert!( pair.is_err() );
+    
+        Ok( () )
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+pub use anyhow::anyhow;
 use my_parser_yaroslav_fetisov::*;
 
-pub fn main() {
+pub fn main() -> anyhow::Result<()> {
     println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
+    let pair = Grammar::parse(Rule::field, "131.13")?
+        .next()
+        .ok_or_else(|| anyhow!("no pair"))?;
+    dbg!(pair);
+    Ok(())
 }
